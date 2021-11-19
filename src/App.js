4ae75a2466e95276d4b52b0ce9ceb6300c53484f
“Vanessa” <vanessa.mercado24@gmail.com>
2021-11-18 18:23:32 -0700

    make btn

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div class="flex-shrink-0">
-      </div>
-      <div>
-        <div class="text-xl font-medium text-black">ChitChat</div>
-        <p class="text-gray-500">You have a new message!</p>
-      </div>
+    <div class="flex h-screen">
+    <div class="m-auto">
+    <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">
+      Mint
+    </button>
     </div>
+  </div>
   );
 }
 
